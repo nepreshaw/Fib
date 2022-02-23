872ccb61a84427a5b14346a28266c9846ce34831
Nicholas Preshaw <nickpreshaw@yahoo.com>
2022-02-23 16:14:43 -0500

    Add project files.

---
+++ Practice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Practice {
+    class Program {
+        static void Main(string[] args) {
+
+            //int sum = 0;
+            //for (int idx = 1; idx < 1000; idx++) {
+            //    if(((idx % 3) == 0) || ((idx % 5) == 0)) {
+            //        sum += idx;
+            //    }
+            //}
+            //Console.WriteLine(sum);
+
+            int fib1 = 1;
+            int fib2 = 1;
+            int result = 0;
+            int summed = 0;
+
+            while(result < 4000000) {
+                if((result % 2) == 0){
+                    summed += result;
+                }
+                result = fib1 + fib2;
+                fib2 = fib1;
+                fib1 = result;
+            }
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
